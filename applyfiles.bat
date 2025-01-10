068173fb17e2bca76c2872cd55aa82e07d164338
nimigeanconstantinion <constantin.nimigean@gmail.com>
2025-01-10 19:36:16 +0200

    first commit

---
+++ applyfiles.bat
@@ -0,0 +1,7 @@
+kubectl apply -f .\pv-pvc.yaml
+kubectl apply -f .\secret.yaml
+kubectl apply -f .\configmap.yaml
+kubectl apply -f .\mysql-statefulset.yaml
+kubectl apply -f .\deployment-kube-land.yaml
+kubectl apply -f .\deployment-api.yaml
+kubectl apply -f .\deployment-client.yaml
